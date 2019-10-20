6ce028b8ab90a2ee507fdcb20bc02321de8aa379
unknown <bhgtankita@gmail.com>
2019-10-20 10:49:55 +0530

    Web App using Flask

---
+++ app.py
@@ -0,0 +1,73 @@
+from __future__ import division, print_function
+import sys
+import os
+import glob
+import re
+import numpy as np
+from flask import Flask, redirect, url_for, request, render_template
+from werkzeug.utils import secure_filename
+from gevent.pywsgi import WSGIServer
+from keras.applications.imagenet_utils import preprocess_input, decode_predictions
+from keras.models import load_model
+from keras.preprocessing import image
+from keras.applications.resnet50 import ResNet50
+
+# initializing flask application
+app = Flask(__name__) 
+
+model_path = 'models/model_resnet50.h5'
+
+# load the model & Download weights
+#model = ResNet50(weights='imagenet')
+#model.save(model_path)
+
+# Load the trained weights
+model = load_model(model_path)
+model._make_predict_function()
+
+print('Model is loaded.')
+
+def model_predict(img_path, model):
+    img = image.load_img(img_path, target_size=(224, 224))
+
+    # Preprocessing the image
+    x = image.img_to_array(img)
+    # x = np.true_divide(x, 255)
+    x = np.expand_dims(x, axis=0)
+
+    # Be careful how your trained model deals with the input
+    # otherwise, it won't make correct prediction!
+    x = preprocess_input(x, mode='caffe')
+
+    preds = model.predict(x)
+    return preds
+
+@app.route('/', methods=['GET'])
+def index():
+    # Main page
+    return render_template('index.html')
+
+@app.route('/predict', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        # Get the file from post request
+        f = request.files['file']
+
+        # Save the file to ./uploads
+        basepath = os.path.dirname(__file__)
+        file_path = os.path.join(
+            basepath, 'uploads', secure_filename(f.filename))
+        f.save(file_path)
+
+        # Make prediction
+        preds = model_predict(file_path, model)
+
+        # Process your result for human
+        # pred_class = preds.argmax(axis=-1)            # Simple argmax
+        pred_class = decode_predictions(preds, top=1)   # ImageNet Decode
+        result = str(pred_class[0][0][1])               # Convert to string
+        return result
+    return None
+
+if __name__ == '__main__':
+    app.run(debug=True)
